6b4f022cde8e9b32c35bbb94c786e49249d97fb9
andre3pazo <61413176+andre3pazo@users.noreply.github.com>
2020-03-30 17:51:49 +0200

    Test.

--- Test/tests/RegisterTest.cpp
+++ Test/tests/RegisterTest.cpp
@@ -6,18 +6,18 @@
 #include "../../Register.h"
 
 TEST(RegisterTest, CorrectActivitiesNumber) {
-auto firstActivity = Activity();
-auto secondActivity = Activity();
-auto thirdActivity = Activity();
-auto forthActivity = Activity();
-auto firstDate = Date();
-auto secondDate = Date();
-auto thirdDate = Date();
-auto forthDate = Date();
-Register firstRegister;
-firstRegister.add(&firstDate, &firstActivity);
-firstRegister.add(&secondDate, &secondActivity);
-firstRegister.add(&thirdDate, &thirdActivity);
-firstRegister.add(&forthDate, &forthActivity);
-ASSERT_EQ(4, firstRegister.getNumActivities());
+    auto firstActivity = Activity();
+    auto secondActivity = Activity();
+    auto thirdActivity = Activity();
+    auto forthActivity = Activity();
+    auto firstDate = Date();
+    auto secondDate = Date();
+    auto thirdDate = Date();
+    auto forthDate = Date();
+    Register firstRegister;
+    firstRegister.add(&firstDate, &firstActivity);
+    firstRegister.add(&secondDate, &secondActivity);
+    firstRegister.add(&thirdDate, &thirdActivity);
+    firstRegister.add(&forthDate, &forthActivity);
+    ASSERT_EQ(4, firstRegister.getNumActivities());
 }
